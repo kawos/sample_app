20ccea9a12106ae8804b850dc73b58e923f18361
Jan Kawulok <jan@kawulok.com.pl>
2013-07-12 15:27:58 +0200

    page title refactor

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,7 +10,11 @@ describe "Static pages" do
     end
     it "should have the right title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => " | Home")
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App")
+    end
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => "Home")
     end
   end
 
